83098d9d12ecd1bb28a29b16a0f447d493afe245
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-06-29 19:53:29 +0530

    bit

---
+++ SwappingUsingBitW.java
@@ -0,0 +1,12 @@
+public class SwappingUsingBitW {
+    public static void main(String[] args) {
+        int a=10,b=15;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println(a+" "+b);
+    }
+
+
+    
+}
