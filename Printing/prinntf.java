3437e0e9a539ed6a9bbac10876d196f19af856da
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-06-29 21:57:08 +0530

    new

---
+++ Printing/prinntf.java
@@ -0,0 +1,17 @@
+package Printing;
+
+public class prinntf {
+    public static void main(String[] args) {
+      /*   int a=10;
+        System.out.printf("%05d",a); */
+        String str="Aryan";      
+        String str1=new String("Aryan");
+        char c[]={'h','e','l','l','o'};
+        String str2=new String(c);
+        byte b[]={65,66,67,68};
+        String str3=new String(b);
+
+        System.out.println(str3);
+    }
+    
+}
