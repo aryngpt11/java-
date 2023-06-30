fe37f6eacd85f2cff47d23abf5f548f559df65c7
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-07-01 00:49:37 +0530

    fact

--- Printing/Factoriall.java
+++ Printing/Factoriall.java
@@ -3,16 +3,13 @@ public class Factoriall{
     public static void main(String[] args) {
         int n;
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the no which we want to find the factorial: ");
+        System.out.print("Enter the no:");
         n=sc.nextInt();
-       /*  if(n==0||n==1){
-            System.out.println("The factorial of 0 and 1 is = 1");
-        } */
-        for (int i=1;i<(n-1);i++){
-            n=n*i;
-        }
-        System.out.println(n);
-        
+        int fact=1;
+        for (int i=1;i<=n;i++){
+            fact=fact*i;
+        }System.out.println("the factorial of:"+" "+n+ "" +"is "+fact);
+        sc.close();
     }
 
 }
