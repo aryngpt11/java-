905ea28824c1013cca11642daf54fcbaae923797
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-06-30 20:10:49 +0530

    while

---
+++ Printing/ExampleWhile.java
@@ -0,0 +1,15 @@
+public class ExampleWhile{
+    public static void main(String[] args) {
+        int i=100;
+        /* while(i<100){
+            System.out.println(i);
+            i=i*2;
+        } */
+        do{
+            System.out.println(i);
+            i=i*2;
+        }
+        while(i<100);
+
+    }
+}
---
+++ Printing/Switchcs.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Switchcs {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no: ");
+        n=sc.nextInt();
+        switch(n){
+            case 1:System.out.println("one");
+            break;
+            case 2:System.out.println("two");
+            break;
+            case 3:System.out.println("three");
+            break;
+            default:System.out.println("Invalid input");
+        }
+    }
+
+    
+}
