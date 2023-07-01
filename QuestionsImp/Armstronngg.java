8b5e0fb8290f97432ae3e699e133c26e9825c796
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-07-01 16:52:25 +0530

    armstrong

---
+++ QuestionsImp/Armstronngg.java
@@ -0,0 +1,27 @@
+package QuestionsImp;
+import java.util.Scanner;
+
+public class Armstronngg {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enetr the number to check the number is armstrong or not: ");
+        n=sc.nextInt();
+        int m=n;
+        int r;
+        int summ=0;
+        while(n>0){
+            r=n%10;
+            summ=summ+r*r*r;
+            n=n/10;
+        }
+        if(summ==m){
+        System.out.println("Yes the no" + m +"is armstrong");
+        }
+        else{
+            System.out.println("No the no" + m +"is armstrong");
+        }
+        sc.close();
+        
+    }   
+}
