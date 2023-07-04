1b722eb999f5cab6118f50ae74b0a3051e6a8e7a
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-07-04 10:36:12 +0530

    rev

---
+++ QuestionsImp/Reversse.java
@@ -0,0 +1,19 @@
+package QuestionsImp;
+import java.util.*;
+
+public class Reversse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number you want to reverse: ");
+        int n=sc.nextInt();
+        int rev=0,r;
+        while(n>0){
+            r=n%10;
+            rev=rev*10+r;
+            n=n/10;
+
+        }
+        System.out.println(rev);
+    }
+    
+}
