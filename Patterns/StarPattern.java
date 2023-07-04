27c1b6826bf5551549a8a4c7c4c505c9768e071f
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-07-05 00:58:13 +0530

    star

---
+++ Patterns/StarPattern.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+
+
+                
+            }
+            System.out.println("");
+        }
+    }
+    
+}
