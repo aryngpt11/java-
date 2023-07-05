1b62467f3888a68c5a793bc2ef762abbd08fe6cd
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-07-05 15:09:29 +0530

    sum of elements of array

---
+++ Arrrayy/SumOfElements.java
@@ -0,0 +1,13 @@
+package Arrrayy;
+
+public class SumOfElements {
+    public static void main(String[] args) {
+        int a[]={10,20,30,40,50,60};
+        int summ=0;
+        for(int i=0;i<a.length;i++){
+            summ=summ+a[i];
+
+        }
+        System.out.println("The sum of arrays elements are: "+summ);
+    }
+}
