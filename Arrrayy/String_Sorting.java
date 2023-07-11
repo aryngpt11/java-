eb36f35c9d63b8d973a90fe68d8e95732f784f75
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-07-11 22:39:41 +0530

    array

---
+++ Arrrayy/String_Sorting.java
@@ -0,0 +1,12 @@
+package Arrrayy;
+
+public class String_Sorting {
+    public static void main(String[] args) {
+        String arr[]={"java","python","pascal","smalltalk","ada","basic"};
+        java.util.Arrays.sort(arr);
+        for(String x:arr){
+            System.out.println(x);
+        }
+    }
+    
+}
