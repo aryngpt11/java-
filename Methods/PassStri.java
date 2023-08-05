fd0aee53236934ce442ef2661d0ede182e8b0cf2
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-08-05 13:50:50 +0530

    java Passing Arguments

---
+++ Methods/PassStri.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class PassStri {
+    void welcome(String n){
+        System.out.println("hello Mr "+n);
+    }
+    public static void main(String[] args) {
+        String s="aryngpt11";
+        PassStri p=new PassStri();
+        p.welcome(s);
+        
+
+    }
+    
+}
